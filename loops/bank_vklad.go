5581010d1fff160a4bf53259873bbbfa69bcb8d5
mks <moiseevko@yandex.ru>
2020-02-16 02:16:06 +0700

    add bank_vklad.go

--- loops/bank_vklad.go
+++ loops/bank_vklad.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var x, y, p float32
+	counter := 0
+	fmt.Print("Enter: ")
+	fmt.Scan(&x, &p, &y)
+	for x < y {
+		x = x + x/100*p
+		fmt.Printf("it is %d year. sum is %v\n", counter, x)
+		counter++
+	}
+	fmt.Println(counter)
 }
