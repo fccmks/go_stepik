d2763d4a71d88bc3c449236a5686c55e6a6f70b4
mks <moiseevko@yandex.ru>
2020-02-17 02:16:01 +0700

    revert

--- simple_tasks.go
+++ simple_tasks.go
@@ -3,12 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	//fmt.Println("Enter numbers: ")
-	//for n := 0; n <= 100; {
-	//	fmt.Scan(&n)
-	//	if n >= 10 && n <= 100 {
-	//		fmt.Println(n)
-	//	}
-	//}
-	fmt.Println(5 % 1)
+	fmt.Println("Enter numbers: ")
+	for n := 0; n <= 100; {
+		fmt.Scan(&n)
+		if n >= 10 && n <= 100 {
+			fmt.Println(n)
+		}
+	}
 }
