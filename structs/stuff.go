d560fe002f67e92b2d59d16bb8960d85db73d3a7
mks <moiseevko@yandex.ru>
2021-05-03 13:32:45 +0700

    struct lesson added

---
+++ structs/stuff.go
@@ -0,0 +1,57 @@
+// В рамках этого урока мы постарались представить себе уже привычные нам переменные
+// и функции, как объекты из реальной жизни. Чтобы закрепить результат мы предлагаем
+// вам небольшую творческую задачу.
+
+// Вам необходимо реализовать структуру со свойствами-полями On, Ammo и Power,
+// с типами bool, int, int соответственно. У этой структуры должны быть методы: Shoot и RideBike,
+// которые не принимают аргументов, но возвращают значение bool.
+
+// Если значение On == false, то оба метода вернут false.
+
+// Делать Shoot можно только при наличии Ammo
+// (тогда Ammo уменьшается на единицу, а метод возвращает true), если его нет, то метод вернет false.
+// Метод RideBike работает также, но только зависит от свойства Power.
+
+// Чтобы проверить, что вы все сделали правильно, вы должны создать указатель на экземпляр
+// этой структуры с именем testStruct в функции main, в дальнейшем программа проверит результат.
+
+package main
+
+import "fmt"
+
+type Stuff struct {
+	On    bool
+	Ammo  int
+	Power int
+}
+
+func (s *Stuff) Shoot() bool {
+	if !s.On {
+		return false
+	}
+	if s.Ammo > 0 {
+		s.Ammo = s.Ammo - 1
+		return true
+	} else {
+		return false
+	}
+}
+
+func (s *Stuff) RideBike() bool {
+	if !s.On {
+		return false
+	}
+	if s.Power > 0 {
+		s.Power = s.Power - 1
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	testStruct := &Stuff{true, 0, 90}
+	fmt.Println("Ammo before:", testStruct.Ammo)
+	testStruct.Shoot()
+	fmt.Println("Ammo after:", testStruct.Ammo)
+}
