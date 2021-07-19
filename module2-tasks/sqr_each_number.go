bc1d234687247bc440c1e9feafa68f736d8615e5
mks <moiseevko@yandex.ru>
2021-07-20 01:08:47 +0700

    solving tasks 1-4

---
+++ module2-tasks/sqr_each_number.go
@@ -0,0 +1,28 @@
+// На вход подается целое число. Необходимо возвести в квадрат каждую цифру
+// числа и вывести получившееся число.
+
+// Например, у нас есть число 9119. Первая цифра - 9. 9 в квадрате - 81.
+// Дальше 1. Единица в квадрате - 1. В итоге получаем 811181
+
+// Sample Input:
+// 9119
+
+// Sample Output:
+// 811181
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var a int
+	fmt.Scan(&a)
+	s := strconv.Itoa(a)
+	for _, v := range s {
+		val, _ := strconv.Atoi(string(v))
+		fmt.Print(val * val)
+	}
+}
